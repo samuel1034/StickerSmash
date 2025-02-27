772958c522d66607c5865a33275933359bc9651f
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2025-02-27 08:35:53 -0400

    Added a Screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,25 +3,34 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
+
     <Tabs
-    screenOptions={{
+      screenOptions={{
         tabBarActiveTintColor: '#ffd33d',
-    }}
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+        },
+      }}
     >
-      <Tabs.Screen 
-      name="index"
-       options={{
-        title:'Home',
-        tabBarIcon: ({ color, focused }) => (
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Sticker Smash',
+          tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
           ),
-        }} 
-       />
-      <Tabs.Screen name="about" 
-      options={{
-        title:'About',
-        tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+        }}
+      />
+      <Tabs.Screen name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
           ),
         }} />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+
+const PlaceholderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label='Choose a photo' />
+        <Button label='Use this photo' />
+
+      </View>
     </View>
   );
 }
@@ -13,9 +24,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
   },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+
 });
